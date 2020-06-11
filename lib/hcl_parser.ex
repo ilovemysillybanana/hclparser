5a52837c1d85ea915b64dad7662b2d67a7646b6d
Jose M. Tobar <ilovemysillybanana@gmail.com>
2020-06-10 22:15:31 -0400

    added ex doc method documentation

--- lib/hcl_parser.ex
+++ lib/hcl_parser.ex
@@ -1,4 +1,20 @@
 defmodule HCLParser do
+
+  @doc """
+  Parses a terraform file, and returns a map structure representing it.
+
+  ## Parameters
+
+    - content: String that represents the content of the terraform file.
+
+  ## Examples
+
+      iex> content = 'provider "google" {}'
+
+      iex> HCLParser.parse(content)
+      {:ok, %{"provider" => %{"google" => {}}}}
+
+  """
   def parse(content) when is_bitstring(content), do: content |> String.to_charlist() |> parse()
   def parse(content) do
     with {:ok, tokens, _} <- :lexer.string(content) do
